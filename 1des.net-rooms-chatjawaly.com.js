62d6814099fff6d8f28f375154cb929bf2b43f80
waleed-qaid <48764757+waleed-qaid@users.noreply.github.com>
2022-12-07 00:27:43 +0300

    Update 1des.net-rooms-chatjawaly.com.js

--- 1des.net-rooms-chatjawaly.com.js
+++ 1des.net-rooms-chatjawaly.com.js
@@ -384,28 +384,28 @@ $("style").last().append(`
 }
 #d2.kdmat5 {
 background-color: #0000 !important;
-background-image: url("https://b.top4top.io/p_2480032wg1.png");
+background-image: url("https://i.ibb.co/k96bjDt/kd10281924d7a561122.png");
 background-size: cover;
 }
 #users.kdmat5 {
-  background-image: url("https://b.top4top.io/p_2480032wg1.png");
+  background-image: url("https://i.ibb.co/k96bjDt/kd10281924d7a561122.png");
   background-size: cover;
   background-color: #fafafa00 !important;
 }
 #settings.kdmat5 {
-  background-image: url("https://b.top4top.io/p_2480032wg1.png");
+  background-image: url("https://i.ibb.co/k96bjDt/kd10281924d7a561122.png");
   background-size: cover;
 }
 #wall.kdmat5 #d2bc {
-  background-image: url("https://b.top4top.io/p_2480032wg1.png");
+  background-image: url("https://i.ibb.co/k96bjDt/kd10281924d7a561122.png");
   background-size: cover;
 }
 #rooms.kdmat5 {
-  background-image: url("https://b.top4top.io/p_2480032wg1.png");
+  background-image: url("https://i.ibb.co/k96bjDt/kd10281924d7a561122.png");
   background-size: cover;
 }
 #chats.kdmat5 {
-  background-image: url("https://b.top4top.io/p_2480032wg1.png");
+  background-image: url("https://i.ibb.co/k96bjDt/kd10281924d7a561122.png");
   background-size: cover;
 }
 #users.kdmat5 .inroom.uzr{border-radius:5px;border:1px solid #fffefe;border-bottom:1px solid #fffefe}
