9bfedb28577ba575b4b3ff79e62de3b49f2a908e
waleed-qaid <48764757+waleed-qaid@users.noreply.github.com>
2023-01-22 10:01:29 +0300

    Update 1des.net-rooms-yallandardsh.com.js

--- 1des.net-rooms-yallandardsh.com.js
+++ 1des.net-rooms-yallandardsh.com.js
@@ -2412,6 +2412,16 @@ margin-left: -12px;
 
 #upro.jnoon1 .light.fl.pro.break{background-image: linear-gradient(225deg,#dfdfdf 0%,#f7f7f7 29%,#f4f4f4 67%,#dbdbdb 100%);}
 
+#mic.jnoon1 #muteall {
+    border-radius: 15px 0px 15px 0px;
+    margin-top: 1px;
+    border: 1px solid;
+    border-image: linear-gradient(45deg, gold, deeppink) 5;
+    clip-path: inset(0px round 15px 0px 15px 0px);
+    filter: hue-rotate(360deg);
+    box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 4px rgb(0 0 0), 0 0 0 0px #000;
+}
+
 #wall.jnoon1 .fitimg.u-pic.borderg {
 height: 52px !important;
 border: 1px solid;
