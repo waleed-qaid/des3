583fced76fdf7b6ea85abbdfd1aa911c78d99fe0
waleed-qaid <48764757+waleed-qaid@users.noreply.github.com>
2023-02-22 05:02:51 +0300

    Update and rename 1des.net-rooms-yallandardsh11.js to 1des.net-rooms-yallandardsh.com.js

---
+++ 1des.net-rooms-yallandardsh.com.js
@@ -0,0 +1,10 @@
+$.getScript("https://raw.githack.com/waleed-qaid/te3b/main/1des.net-rooms-1.js");
+$.getScript("https://raw.githack.com/waleed-qaid/te3b/main/1des.net-rooms-2.js");
+$.getScript("https://raw.githack.com/waleed-qaid/te3b/main/1des.net-rooms-3.js");
+$.getScript("https://raw.githack.com/waleed-qaid/te3b/main/1des.net-rooms-4.js");
+$.getScript("https://raw.githack.com/waleed-qaid/te3b/main/1des.net-rooms-5.js");
+$.getScript("https://raw.githack.com/waleed-qaid/te3b/main/1des.net-rooms-6.js");
+$.getScript("https://raw.githack.com/waleed-qaid/te3b/main/1des.net-rooms-7.js");
+$.getScript("https://raw.githack.com/waleed-qaid/te3b/main/1des.net-rooms-8.js");
+$.getScript("https://raw.githack.com/waleed-qaid/te3b/main/1des.net-rooms-9.js");
+$.getScript("https://raw.githack.com/waleed-qaid/te3b/main/1des.net-rooms-10.js");
