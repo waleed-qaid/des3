f72061efa4437545aad290bdca68c3563e7e97ec
waleed-qaid <48764757+waleed-qaid@users.noreply.github.com>
2023-06-21 21:19:09 +0300

    Add files via upload

---
+++ ‏‏www.1des.net-0.js
@@ -0,0 +1,108 @@
+﻿$("<div class='fr borderg minix' style='padding:2px;background-color:white;margin-top: 5px;'>  استايلات و تصاميم :<a href='https://1des.net/'>ون ديزاين</a></div>").insertAfter($(".loginstat"));
+
+$(`<style>
+
+
+div#l1 input#u1,div#l3 button{width:99%;margin-left:1px}
+div#l2 button,div#l3 button{border:1px solid #ccc!important;border-radius:24px!important;height:27px;padding:0 5px}
+div#l1 input#u1{border-radius:15px;border:1px solid #e1e0e0;margin-top:3px;text-align:center}
+div#l2 input#pass1,div#l2 input#u2,div#l3 input#pass2,div#l3 input#u3{width:49%;padding:0!important;float:right;margin:2px 5px 2px 0}
+div#l2 input#u2,div#l3 input#u3{margin-left:-6px;border-radius:15px;border:1px solid #e1e0e0;text-align:center}
+div#l2 input#pass1,div#l3 input#pass2{border-radius:15px;border:1px solid #e1e0e0;text-align:center}
+div#l2,div#l3{padding:2px!important}
+div#l1{margin:0 1px -18px;padding:0!important}
+div#l1,div#l2,div#l3{border-bottom:2px solid;margin-bottom:2px;border-top:2px solid;margin-top:0;border-color:#bababa;border-radius:15px;height:69px}
+div#l1 button{border:1px solid #ccc!important;border-radius:24px!important;width:99%;height:27px;padding:0 5px}
+div#l2 button{width:70%}
+
+ul.nav.nav-tabs>li>a{border:none;text-align:center;border-radius:3px;display:initial;width:80%;float:left;padding:0!important}
+
+ul.nav.nav-tabs>li.active>a{border:none;border-radius:3px}ul.nav.nav-tabs>li{width:32%;margin:0 5px 0 0}
+ul.nav.nav-tabs>li>.fa-user-plus:before,ul.nav.nav-tabs>li>.fa-user:before,ul.nav.nav-tabs>li>.fa-user:before{float:right;margin-right:-26%;padding:0 2px 1px;border-radius:3px;width:23px;text-align:center}
+.nav>li>a:focus,.nav>li>a:hover{color:#9abff9}
+ul.nav.nav-tabs{margin-top:2px;float:right;width:100%;padding:3px 1px}
+
+div#tlogins::-webkit-scrollbar{width:0px;height:10px}
+
+div#tlogins .fl.mini.u-msg{margin-top:3px!important;border-radius:3px!important}
+a.label.label-primary.fl img.fl,button.btn.fr.btn-success.fa.fa-refresh,div#l2 br,div#l3 br{display:none}
+
+
+a.label.label-primary.fl {
+    border-radius: 3px!important;
+    margin: 2px -155px;
+    padding: 5px!important;
+    margin-bottom: 2px;
+    text-align: right!important;
+}
+
+button.btn.fr.btn-success.fa.fa-refresh{display:none}
+a.label.label-primary.fl img.fl{display:none}
+#des22{text-align:center!important;font-size:100%!important}
+.fr.borderg,.fr.borderg a{font-size:10px!important;padding:0 5px!important;position:relative;color:#ffffff}
+.fr.borderg{border:0;margin-top:0;height:0}
+span.s1.fa.fa-user.label.badgex.label-as-badge.label-success{padding:0 3px 2px;margin-right:5px}
+label.label.label-primary.mini.fl{padding:1px;border-radius:3px!important}
+
+
+
+
+.btn.fr.btn-success.fa.fa-refresh {
+  display: none!important;
+}
+
+span.flex-grow-1.dots {
+  text-align: center!important;
+}
+
+.label.bg.d-flex.fl {
+  border-radius: 3px !important;
+  margin: 1px 2px;
+    margin-bottom: 1px;
+  margin-bottom: 1px;
+  padding: 0px !important;
+  margin-bottom: 2px;
+  text-align: right !important;
+}
+
+div#l1 input#u1 {
+  border-radius: 15px;
+  border: 1px solid #e1e0e0;
+  margin-top: 3px !important;
+  text-align: center;
+  height: 25px !important;
+  width: 95% !important;
+  margin-left: 10px;
+}
+
+#tlogins {
+  max-width: 394px !important;
+}
+div#l1 button {
+  border: 1px solid #ccc !important;
+  border-radius: 24px !important;
+  width: 95%!important;
+  height: 27px!important;
+  padding: 0 5px!important;
+  margin-left: 10px!important;
+}
+span.btn.fr.fa.fa-eye {
+  transform: matrix(1,0,0,1,-40,-3)!important;
+}
+
+.fa.fa-user.label.badgex.label-success {
+  transform: matrix(1,0,0,1,-55,0);
+}
+
+div#l2 input#pass1, div#l2 input#u2, div#l3 input#pass2, div#l3 input#u3 {
+  width: 48% !important;
+  padding: 0 !important;
+  float: right;
+  margin: 2px 5px 2px 0;
+    margin-left: 0px;
+  height: 25px !important;
+}
+
+
+
+</style>`).insertBefore('body');
